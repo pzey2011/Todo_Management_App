26e4fdf81e774aee54a485cc83a186cb6d843ec2
Peyman Zeynali <peymanzeynali@yahoo.com>
2022-01-27 16:36:54 +0330

    simple input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import {useState,Component} from 'react';
+import { useState } from "react";
 import "./styles.css";
 import InputField from "./components/InputField";
-type Props = {};
 
-type State = {
-  todo: string;
-};
-class App extends Component<Props,State> {
-
-  state = {};
-  setTodoInput = (input:string) =>{
+const App = () => {
+  const [todo, setTodo] = useState<string>("");
+  const setTodoInput = (input: string) => {
     setTodo(input);
-  }
-  return(){
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <InputField value={todo} setValue={setTodoInput} placeholder={"Enter your password"}/>   
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <InputField
+        value={todo}
+        setValue={setTodoInput}
+        placeholder={"Enter your password"}
+      />
+    </div>
+  );
+};
 
 export default App;
